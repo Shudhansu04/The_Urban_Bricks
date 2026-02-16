ce3cbcc298466e5d38bd846c2bea71c4eec971b2
SHUDHANSU PANDEY <shudhansupandey2004@gmail.com>
2026-02-16 19:13:07 +0530

    Refactor cookie settings for production environment and update CORS origin to localhost

--- backend/src/index.js
+++ backend/src/index.js
@@ -22,7 +22,7 @@ const app = express();
 app.use(helmet());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://the-urban-bricks.vercel.app",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
   })
 );
